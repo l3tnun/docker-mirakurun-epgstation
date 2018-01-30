6c593d0dc6e6d9ce63064092512629f12a6f69f9
l3tnun <l3tnun@gmail.com>
2018-01-30 19:21:03 +0900

    fix

--- epgstation/config/enc.sh
+++ epgstation/config/enc.sh
@@ -13,8 +13,8 @@ function getHeight() {
 }
 
 if [ `getHeight` -gt 720 ]; then
-    $ffmpeg_cmd -dual_mono_mode $mode -vaapi_device /dev/dri/renderD128 -hwaccel vaapi -hwaccel_output_format vaapi -i "$INPUT" -vf 'format=nv12|vaapi,hwupload,deinterlace_vaapi,scale_vaapi=w=1280:h=720' -c:v h264_vaapi -level 40 -qp 21 -aspect 16:9 -acodec aac -ar 48000 -ab 192k -ac 2 "$OUTPUT"
+    $ffmpeg_cmd -dual_mono_mode $mode -vaapi_device /dev/dri/renderD128 -i "$INPUT" -vf 'format=nv12|vaapi,hwupload,deinterlace_vaapi,scale_vaapi=w=1280:h=720' -c:v h264_vaapi -level 40 -qp 21 -aspect 16:9 -acodec aac -ar 48000 -ab 192k -ac 2 "$OUTPUT"
 else
-    $ffmpeg_cmd -dual_mono_mode $mode -vaapi_device /dev/dri/renderD128 -hwaccel vaapi -hwaccel_output_format vaapi -i "$INPUT" -vf 'format=nv12|vaapi,hwupload,deinterlace_vaapi,scale_vaapi=w=720:h=480' -c:v h264_vaapi -level 40 -qp 21 -aspect 16:9 -acodec aac -ar 48000 -ab 128k -ac 2 "$OUTPUT"
+    $ffmpeg_cmd -dual_mono_mode $mode -vaapi_device /dev/dri/renderD128 -i "$INPUT" -vf 'format=nv12|vaapi,hwupload,deinterlace_vaapi,scale_vaapi=w=720:h=480' -c:v h264_vaapi -level 40 -qp 21 -aspect 16:9 -acodec aac -ar 48000 -ab 128k -ac 2 "$OUTPUT"
 fi
 

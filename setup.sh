65cf9e2b44e0b009798ee7596d88a1a5dfad8f8b
burningmortal <seirax@protonmail.com>
2022-08-13 13:22:15 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git clone https://github.com/l3tnun/docker-mirakurun-epgstation.git
+git clone https://github.com/burningmortal/docker-mirakurun-epgstation.git
 cd docker-mirakurun-epgstation
 cp docker-compose-sample.yml docker-compose.yml
 cp epgstation/config/enc.js.template epgstation/config/enc.js
@@ -8,4 +8,4 @@ cp epgstation/config/config.yml.template epgstation/config/config.yml
 cp epgstation/config/operatorLogConfig.sample.yml epgstation/config/operatorLogConfig.yml
 cp epgstation/config/epgUpdaterLogConfig.sample.yml epgstation/config/epgUpdaterLogConfig.yml
 cp epgstation/config/serviceLogConfig.sample.yml epgstation/config/serviceLogConfig.yml
-docker-compose run --rm -e SETUP=true mirakurun
+docker compose run --rm -e SETUP=true mirakurun
